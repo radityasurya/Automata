e41c13aa20e8b7ae4457a111e382ab50f0b55f20
radityasurya <radityasurya@hotmail.com>
2016-11-15 00:10:03 +0100

    read the loaded files

--- Automata/AutomataManager.cs
+++ Automata/AutomataManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
 
 namespace Automata
 {
@@ -13,8 +15,7 @@ namespace Automata
         // Variables
         private List<string> loadedFileList = new List<string>();
         private List<char> alphabets = new List<char>();
-        private List<char> states = new List<char>();
-        private List<char> finalStates = new List<char>();
+        private List<State> states = new List<State>();
 
         public AutomataManager()
         {
@@ -30,6 +31,8 @@ namespace Automata
             getAlphabets();
             getStates();
             getFinalState();
+
+            states.Count();
         }
 
         public void getAlphabets()
@@ -40,12 +43,43 @@ namespace Automata
         public void getStates()
         {
             string result = loadedFileList.Find(item => item.Contains("states"));
+            result = result.Split(':').Last();
 
+            string[] _state = result.Split(',');
+
+            foreach (var s in _state)
+            {
+                states.Add(new State(s));
+            }
         }
 
         public void getFinalState()
         {
             string result = loadedFileList.Find(item => item.Contains("final"));
+            result = result.Split(':').Last();
+            result = result.Trim();
+
+            List<string> _state = new List<string>();
+
+            if (result.Contains(','))
+            {
+                _state = result.Split(',').ToList();
+            } else
+            {
+                _state.Add(result);
+            }
+
+            foreach (var _s in _state)
+            {
+
+                foreach (var s in states)
+                {
+                    if (s.Name.Equals(_s))
+                    {
+                        s.isFinal = true;
+                    }
+                }
+            }
         }
 
     }
---
+++ Automata/State.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automata
+{
+    public class State
+    {
+        public string Name { get; private set; }
+        public bool isFinal { get; set; }
+
+        public State(string name)
+        {
+            this.Name = name;
+            this.isFinal = false;
+        }
+    }
+}
